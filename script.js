df8e19965bd9181cfcc96029c48f277f186df1e9
fazeelakarim423-hash <fazeelakarim423@gmail.com>
2026-07-22 02:40:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Portfolio');
+window.addEventListener("scroll", function () {
+    const btn = document.getElementById("topBtn");
+
+    if (window.scrollY > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+document.getElementById("topBtn").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Thank you! Your message has been received.");
+});
